d5e78da52a111ee4443263f7cef97c6992109469
yuecheng03960 <y.cheng03960@gmail.com>
2024-06-10 18:07:17 -0700

    update comment context

--- src/main/resources/sql/petropolis.user.sql
+++ src/main/resources/sql/petropolis.user.sql
@@ -4,21 +4,21 @@ USE petropolis;
 
 # create table user
 CREATE TABLE IF NOT EXISTS `t_user` (
-    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT '物理主键, user id',
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'Primary key, user id',
 
-    `user_name` VARCHAR(64) NOT NULL COMMENT '平台登陆用户名',
-    `password` VARCHAR(32) NOT NULL COMMENT '加密后的登陆密码; 约定全大写; MD5(MD5(source password) + password_salt)',
-    `password_salt` VARCHAR(32) NOT NULL COMMENT '32 CHAR UUID; 密码加密的盐; SELECT UPPER(REPLACE(UUID(),"-",""))',
-    `email_not_verified` VARCHAR(320) NOT NULL DEFAULT '' COMMENT '未经验证的邮箱',
-    `user_email` VARCHAR(320) NOT NULL COMMENT '验证通过的邮箱, 可以用于登陆; First Set UPPER(REPLACE(UUID(),"-",""))',
+    `user_name` VARCHAR(64) NOT NULL COMMENT 'User Name',
+    `password` VARCHAR(32) NOT NULL COMMENT 'Encrypted login password; Convention all caps; MD5(MD5(source password) + password_salt)',
+    `password_salt` VARCHAR(32) NOT NULL COMMENT '32 CHAR UUID; Password encrypted salt; SELECT UPPER(REPLACE(UUID(),"-",""))',
+    `email_not_verified` VARCHAR(320) NOT NULL DEFAULT '' COMMENT 'Unverified Email',
+    `user_email` VARCHAR(320) NOT NULL COMMENT 'Verified Email can be used for login; First Set UPPER(REPLACE(UUID(),"-",""))',
 
-    `update_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-    `create_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    `delete_time` BIGINT NOT NULL DEFAULT 0 COMMENT '软删除时间戳',
+    `update_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'update time',
+    `create_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'create time',
+    `delete_time` BIGINT NOT NULL DEFAULT 0 COMMENT 'Soft delete timestamp',
 
     UNIQUE KEY `unique_user_name` (`user_name`, `delete_time`),
     UNIQUE KEY `unique_user_email` (`user_email`, `delete_time`)
-) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4 COMMENT = '用户表';
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4 COMMENT = 'user table';
 
 # First Insert
 # INSERT INTO t_user (user_name, password, password_salt, email_not_verified, user_email) VALUES (?1, ?2, ?3, ?4, UPPER(REPLACE(UUID(),"-","")))
