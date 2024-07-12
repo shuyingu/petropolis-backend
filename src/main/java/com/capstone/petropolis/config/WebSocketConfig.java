9d4455699106c956fe813ac94b73a1eb6943a69a
JueqiCai <jqtsai97@gmail.com>
2024-07-11 19:24:19 -0700

    update websocket endpoint and allow original Websocket

--- src/main/java/com/capstone/petropolis/config/WebSocketConfig.java
+++ src/main/java/com/capstone/petropolis/config/WebSocketConfig.java
@@ -17,6 +17,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/chat-websocket").withSockJS();
+        registry.addEndpoint("/app/chat-websocket").setAllowedOrigins("*");;
     }
 }
